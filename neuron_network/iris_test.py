c7842234ceafccf1619d5c6b44b0ba840e01df52
EugenRommel <napoleon_hewp@hotmail.com>
2023-04-06 21:46:22 +0800

    Continue to progress with the book# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Your branch is up to date with 'origin/master'.## Changes to be committed:#	new file:   chromosome.py#	new file:   data_point.py#	new file:   genetic_algorithm.py#	new file:   governors.py#	new file:   kmeans.py#	new file:   list_compression.py#	new file:   mj.py#	new file:   neuron_network/iris_test.py#	new file:   neuron_network/layer.py#	new file:   neuron_network/network.py#	new file:   neuron_network/neuron.py#	new file:   neuron_network/util.py#	new file:   send_more_money2.py#	new file:   simple_equation.py## Untracked files:#	.idea/#	__pycache__/#	missionaries.py#

---
+++ neuron_network/iris_test.py
@@ -0,0 +1,37 @@
+import csv
+from random import shuffle
+from typing import List
+
+from network import Network
+from util import normalize_by_feature_scaling
+
+
+def iris_interpret_output(output: List[float]) -> str:
+    if max(output) == output[0]:
+        return "Iris-setosa"
+    elif max(output) == output[1]:
+        return "Iris-versicolor"
+    else:
+        return "Iris-virginica"
+
+
+if __name__ == "__main__":
+    iris_parameters: List[List[float]] = []
+    iris_classifications: List[List[float]] = []
+    iris_species: List[str] = []
+    with open('iris.csv', mode='r') as iris_file:
+        irises: List = list(csv.reader(iris_file))
+        shuffle(irises)
+        for iris in irises:
+            parameters: List[float] = [float(n) for n in iris[0:4]]
+            iris_parameters.append(parameters)
+            species: str = iris[4]
+            if species == "Iris-setosa":
+                iris_classifications.append([1.0, 0.0, 0.0])
+            elif species == "Iris-versicolor":
+                iris_classifications.append([0.0, 1.0, 0.0])
+            else:
+                iris_classifications.append([0.0, 0.0, 1.0])
+            iris_species.append(species)
+        normalize_by_feature_scaling(iris_parameters)
+    iris_network: Network = Network([4, 6, 3], 0.3)
